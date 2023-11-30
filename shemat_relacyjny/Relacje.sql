2722477657872b5d6718df3a950508c636ec4150
jjulkakulkaa <jaromirska2002@gmail.com>
2023-11-30 19:01:26 +0100

    add 'Zamowione_ksiazki' 'Wartosc_zamowienia'

--- shemat_relacyjny/Relacje.sql
+++ shemat_relacyjny/Relacje.sql
@@ -3,8 +3,7 @@ create table Klienci(
     imie varchar2(50) not null,
     nazwisko varchar2(50) not null,
     data_urodzenia date not null,
-    adres varchar2(100) not null,
-    karta_biblioteczna number(9)
+    adres varchar2(100) not null
 );
 
 
@@ -13,15 +12,12 @@ dodatkowa tabela na klientow co maja karte biblio
 w tabeli wypozyczenia zamiast powiazywac z karta_biblioteczna z tabeli Klienci powiazuja sie z karta biblio z tej tabeli
 */
 
-create table Karty_biblioteczne_klientow(
-    karta_biblioteczna
-        references Klienci(karta_biblioteczna)
-        not null,
+create table Karty_biblioteczne(
+    karta_biblioteczna number(9)
+        primary key,
     pesel 
-        references Klienci(pesel),
-    primary key(
-        karta_biblioteczna,
-    )
+        references Klienci(pesel)
+        not null unique
 );
 
 create table Pracownicy(
@@ -85,7 +81,7 @@ create table Wypozyczenia(
     data_wypozyczenia date not null,
     data_zwrotu date,
     karta_biblioteczna
-        references Karty_biblioteczne_klientow(karta_biblioteczna) not null,
+        references Karty_biblioteczne(karta_biblioteczna) not null,
     id_obslugujacego 
         references Pracownicy(pesel) not null,
     id_ksiazki 
@@ -109,17 +105,25 @@ create table Wypozyczenia(
 -- );
 
 create table Zamowienia(
-    numer number(9) primary key,
+    numer_zamowienia number(9) primary key,
     data_zamowienia date not null,
-    wartosc_zamowienia number(6,2) not null,
     status in('w trakcie realizacji', 'zrealizowane'),
     id_klienta  
         references Klienci(pesel) not null,
     id_obslugujacego 
-        references Pracownicy(pesel) not null,
-    id_ksiazki 
-        references Ksiazki(id_ksiazki) not null
+        references Pracownicy(pesel) not null
 );
+/*blokowanie zamowienia ksiazki bez ceny???*/
+create table Zamowione_ksiazki(
+    numer_zamowienia 
+        references Zamowienia(numer_zamowienia) not null,
+    id_ksiazki 
+        references Ksiazki(id_ksiazki) not null,
+    primary key(
+        numer_zamowienia,
+        id_ksiazki)
+);
+
 
 create table Opinie_klientow(
     tresc varchar2(300) not null,
@@ -183,7 +187,7 @@ before insert on Zamowienia
 for each row
 begin
     select numer_zamowienia_seq.nextval
-    into :new.numer
+    into :new.numer_zamowienia
     from dual;
 end;
 /
@@ -204,7 +208,7 @@ create or replace package biblioteka_pkg is
     );
 
     -- Funkcja do obliczania sumy wartości zamówień dla danego klienta
-    function suma_wartosci_zamowien(v_pesel number) return number;
+    function wartosc_zamowienia(v_pesel number) return number;
 end biblioteka_pkg;
 /
 
@@ -222,29 +226,29 @@ create or replace package body biblioteka_pkg is
         values (v_pesel, v_imie, v_nazwisko, v_data_urodzenia, v_adres, v_karta_biblioteczna);
     end dodaj_klienta;
 
-    function suma_wartosci_zamowien(v_pesel number) return number is
+    function wartosc_zamowienia(v_nr_zamowienia number) return number is
         v_suma number := 0;
     begin
-        select sum(wartosc_zamowienia)
+        select sum(wartosc_ksiazki)
         into v_suma
-        from Zamowienia
-        where id_klienta = v_pesel;
+        from Zamowione_ksiazki
+        where numer_zamowienia = v_nr_zamowienia;
 
         return v_suma;
-    end suma_wartosci_zamowien;
+    end wartosc_zamowienia;
 end biblioteka_pkg;
 /
 
 -- Indeksy
 create unique index klienci_i on Klienci(pesel);
-create unique index kart_bibliotecznych_i on Karty_biblioteczne_klientow(karta_biblioteczna);
+create unique index kart_bibliotecznych_i on Karty_biblioteczne(karta_biblioteczna);
 create unique index pracownicy_i on Pracownicy(pesel);
 create unique index gatunki_i on Gatunki(nazwa);
 create unique index wydawnictwa_i on Wydawnictwa(nazwa);
 create unique index autorzy_i on Autorzy(id_autora);
 create unique index ksiazki_i on Ksiazki(id_ksiazki);
 create unique index wypozyczenia_i on Wypozyczenia(id_wypozyczenia);
-create unique index zamowienia_i on Zamowienia(numer);
+create unique index zamowienia_i on Zamowienia(numer_zamowienia);
 create index opinie_i on Opinie_klientow(id_klienta, id_ksiazki);
 create index promocje_i on Promocje(id_ksiazki, wartosc);
 
